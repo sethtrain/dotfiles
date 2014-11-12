6d385e151acfb724349460c48c72b90ca1738acf
Seth Buntin <seth.buntin@gmail.com>
2014-11-12 09:40:36 -0600

    remove comments from devops.session.sh

--- tmuxifier-layouts/devops.session.sh
+++ tmuxifier-layouts/devops.session.sh
@@ -1,15 +1,6 @@
-# Set a custom session root path. Default is `$HOME`.
-# Must be called before `initialize_session`.
 session_root "~/projects/cicayda/devops"
 
-# Create session with specified name if it does not already exist. If no
-# argument is given, session name will be based on layout file name.
 if initialize_session "devops"; then
-
-  # Create a new window inline within session layout definition.
-  #new_window "misc"
-
-  # Load a defined window layout.
   load_window "vim"
   load_window "shell"
   load_window "socks"
@@ -17,10 +8,7 @@ if initialize_session "devops"; then
   load_window "devops-vagrant"
   load_window "monolith"
 
-  # Select the default active window on session creation.
   select_window 1
-
 fi
 
-# Finalize session creation and switch/attach to it.
 finalize_and_go_to_session
