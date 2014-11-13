106708da9bedd3be634bdc9b383a6f43818df286
Seth Buntin <seth.buntin@gmail.com>
2014-11-13 13:30:01 -0600

    fix tmuxifier logs layout cmd position and split distance

--- tmuxifier-layouts/monolith-logs.window.sh
+++ tmuxifier-layouts/monolith-logs.window.sh
@@ -1,10 +1,10 @@
 new_window "logs"
 
 # Split window into panes.
-split_v 50
+split_v 20
 
-run_cmd "ssh dev \"tail -f /var/log/monolith/monolith.log\""
-run_cmd "ssh dev \"tail -f /var/log/monolith/appserver.upstart.log\"" 1
+run_cmd "ssh dev \"tail -f /var/log/monolith/monolith.log\"" 1
+run_cmd "ssh dev \"tail -f /var/log/monolith/appserver.upstart.log\"" 2
 
 # Set active pane.
-select_pane 0
+select_pane 1
