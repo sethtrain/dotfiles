5cd3c4d6e6cc16dfa5286e99cd20a8c5a03cebe0
Seth Buntin <seth.buntin@gmail.com>
2015-06-29 22:32:44 -0500

    initial start of nbuild tmux script

---
+++ tmux-scripts/nbuild.sh
@@ -0,0 +1,21 @@
+SESSION=nbuild
+PROJECT_DIR=~/projects/nb/nbuild
+
+cd $PROJECT_DIR
+
+tmux -2 new-session -d -s $SESSION
+tmux send-keys "cd $PROJECT_DIR" C-m C-l
+tmux rename-window "shell"
+
+tmux new-window -t$SESSION:2 -c $PROJECT_DIR -n "vim"
+tmux send-keys "vim" C-m
+
+# tmux new-window -t$SESSION:3 -c $PROJECT_DIR -n "console"
+# tmux send-keys "rails c" C-m C-l
+
+# tmux new-window -t$SESSION:4 -c $PROJECT_DIR -n "server"
+# tmux send-keys C-l "rails s" C-m
+
+tmux select-window -t$SESSION:1
+tmux -2 attach-session -t $SESSION
+
