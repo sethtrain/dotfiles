1426f16597e4f65a03c0fcc727e9503d1a72ee7a
Seth Buntin <seth.buntin@gmail.com>
2015-07-07 17:32:12 +0000

    update nbuild connecter to attach if it already exists

--- tmux-scripts/nbuild.sh
+++ tmux-scripts/nbuild.sh
@@ -3,19 +3,24 @@ PROJECT_DIR=/nbuild
 
 cd $PROJECT_DIR
 
-tmux -2 new-session -d -s $SESSION
-tmux send-keys "cd $PROJECT_DIR" C-m C-l
-tmux rename-window "shell"
+if (tmux has-session -t "$SESSION" 2> /dev/null); then
+  echo "Session $SESSION exists."
+  tmux attach -t $SESSION
+else
+  echo "Starting $SESSION session."
+  tmux -2 new-session -d -s $SESSION
+  tmux send-keys "cd $PROJECT_DIR" C-m C-l
+  tmux rename-window "shell"
 
-tmux new-window -t$SESSION:2 -c $PROJECT_DIR -n "vim"
-tmux send-keys "vim" C-m
+  tmux new-window -t$SESSION:2 -c $PROJECT_DIR -n "vim"
+  tmux send-keys "vim" C-m
 
-tmux new-window -t$SESSION:3 -c $PROJECT_DIR -n "console"
-tmux send-keys "rails c" C-m C-l
+  tmux new-window -t$SESSION:3 -c $PROJECT_DIR -n "console"
+  tmux send-keys "rails c" C-m C-l
 
-tmux new-window -t$SESSION:4 -c $PROJECT_DIR -n "server"
-tmux send-keys C-l "rails s" C-m
+  tmux new-window -t$SESSION:4 -c $PROJECT_DIR -n "server"
+  tmux send-keys C-l "rails s" C-m
 
-tmux select-window -t$SESSION:1
-tmux -2 attach-session -t $SESSION
-
+  tmux select-window -t$SESSION:1
+  tmux -2 attach-session -t $SESSION
+fi
