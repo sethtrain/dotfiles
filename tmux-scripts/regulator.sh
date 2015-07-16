212d99eca2ea76ba517e33868962c26984ad00ef
Seth Buntin <seth.buntin@gmail.com>
2015-07-16 07:46:12 -0700

    handle reattaching to regulator tmux session correctly

--- tmux-scripts/regulator.sh
+++ tmux-scripts/regulator.sh
@@ -3,12 +3,16 @@ PROJECT_DIR=/regulator
 
 cd $PROJECT_DIR
 
-tmux -2 new-session -d -s $SESSION
-tmux rename-window "shell"
+if (tmux has-session -t "$SESSION" 2> /dev/null); then
+  echo "Session $SESSION exists."
+  tmux attach -t $SESSION
+else
+  tmux -2 new-session -d -s $SESSION
+  tmux rename-window "shell"
 
-tmux new-window -t$SESSION:2 -c $PROJECT_DIR -n "vim"
-tmux send-keys "vim" C-m
+  tmux new-window -t$SESSION:2 -c $PROJECT_DIR -n "vim"
+  tmux send-keys "vim" C-m
 
-tmux select-window -t$SESSION:1
-tmux -2 attach-session -t $SESSION
-
+  tmux select-window -t$SESSION:1
+  tmux -2 attach-session -t $SESSION
+fi
